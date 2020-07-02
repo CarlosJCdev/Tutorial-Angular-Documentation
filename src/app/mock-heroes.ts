ac71537602c8f4c3b78664c7682cf4ad35c52a42
Carlos <carlos4561059919@gmail.com>
2020-07-02 13:18:58 -0500

    Documentation-Angular-TourHéroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero} from './hero';
+
+export const HEROES: Hero[]= [
+  {id: 11, name: 'Dr Nice'},
+  {id: 12, name: 'Batman'},
+  {id: 13, name: 'Superman'},
+  {id: 14, name: 'Flash'},
+  {id: 15, name: 'Arrrow'},
+  {id: 16, name: 'Wonder Woman'}
+];
